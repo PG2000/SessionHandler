780b096560241f614640ffa640aeb4b8c07774b1
pg2000 <pg@webologen.de>
2015-03-31 12:46:52 +0200

    Removed copyright notice

--- src/SessionHandler/RedisSessionHandler.php
+++ src/SessionHandler/RedisSessionHandler.php
@@ -1,24 +1,10 @@
 <?php
 
-/*
- * This file is part of the SncRedisBundle package.
- *
- * (c) Henrik Westphal <henrik.westphal@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace PG2000\SessionHandler;
 
 /**
  * Redis based session storage with session locking support
- *
- * @author Justin Rainbow <justin.rainbow@gmail.com>
- * @author Jordi Boggiano <j.boggiano@seld.be>
- * @author Henrik Westphal <henrik.westphal@gmail.com>
- * @author Maurits van der Schee <maurits@vdschee.nl>
- */
+*/
 class RedisSessionHandler implements \SessionHandlerInterface
 {
     /**
--- tests/SessionHandler/RedisSessionHandlerTest.php
+++ tests/SessionHandler/RedisSessionHandlerTest.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the SncRedisBundle package.
- *
- * (c) Henrik Westphal <henrik.westphal@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace PG2000\SessionHandler\Tests;
 
 use PG2000\SessionHandler\RedisSessionHandler;
